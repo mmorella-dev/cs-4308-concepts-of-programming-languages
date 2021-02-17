828aad4e02315bfc7d1799652a5e506ea8b7b484
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-17 11:36:14 -0500

    Add updated SCL programs

---
+++ module-03/scl/scl.h
@@ -0,0 +1,11 @@
+/* Scientific Computational Language (SCL) language */
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>     
+#include <math.h>   
+ 
+// #include "basic_lib.h" 
+
+enum Boolean {false, true};
+typedef enum Boolean bool;
+
